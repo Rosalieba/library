8193edf3586f2af746099a11ff07f0d5c04cc004
Silva Veronique <veronique.silva@bbcag.ch>
2024-07-04 14:01:55 +0200

    Added simple code to Controller and Service of subject

--- src/main/java/com/books/library/subject/SubjectController.java
+++ src/main/java/com/books/library/subject/SubjectController.java
@@ -1,4 +1,21 @@
 package com.books.library.subject;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/subject")
 public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    public SubjectController(SubjectService subjectService) {
+        this.subjectService = subjectService;
+    }
+
+    @GetMapping
+    public List<String> getSubjects() {return subjectService.getSubjects(); }
 }
--- src/main/java/com/books/library/subject/SubjectService.java
+++ src/main/java/com/books/library/subject/SubjectService.java
@@ -1,4 +1,12 @@
 package com.books.library.subject;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SubjectService {
+
+    public List<String> getSubjects() {return List.of("Hello", "Subjects", ":-)");}
+
 }

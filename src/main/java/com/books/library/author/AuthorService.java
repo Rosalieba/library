1450764448f765172c68820c6db41ceea353cdaf
Silva Veronique <veronique.silva@bbcag.ch>
2024-07-10 18:01:50 +0200

    Added instantiation of Book and Author object

--- src/main/java/com/books/library/author/AuthorService.java
+++ src/main/java/com/books/library/author/AuthorService.java
@@ -1,12 +1,25 @@
 package com.books.library.author;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
 
-    public List<String> getAuthors() {
-        return List.of("Hi", "Authors", ":-)");
+    public List<Author> getAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public void createAuthor(String forename, String surname) {
+        Author author = new Author();
+        author.setForename(forename);
+        author.setSurname(surname);
+        this.authorRepository.save(author);
     }
 }
--- src/main/java/com/books/library/book/BookService.java
+++ src/main/java/com/books/library/book/BookService.java
@@ -1,5 +1,7 @@
 package com.books.library.book;
 
+import com.books.library.author.Author;
+import com.books.library.author.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,23 +9,30 @@ import java.util.List;
 
 @Service
 public class BookService {
-    private final BookRepository bookRepository;
-
     @Autowired
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
+
+
 
     public List<Book> getBooks() {
         return this.bookRepository.findAll();
     }
 
-    public void createBook(Book book) {
+    public void createBook(String title, String summary, List<Integer> authorIds)
+    {
+        Book book = new Book();
+        book.setTitle(title);
+        book.setSummary(summary);
+        for (Integer authorId:authorIds) {
+            Author author = authorRepository.findById(authorId).orElse(null);
+            if (author != null){
+                book.getAuthors().add(author);
+            } else {
+                //exception handling
+            }
+        }
        this.bookRepository.save(book);
     }
-
-
-    //List.of("Hi", "Books", ":-)");
-
-
 }

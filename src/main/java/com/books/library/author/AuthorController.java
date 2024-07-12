3746ee28b889d52de8a052e8e1300b26a182de28
Silva Veronique <veronique.silva@bbcag.ch>
2024-07-12 19:04:47 +0200

    Upadated createAuthor()

--- src/main/java/com/books/library/author/AuthorController.java
+++ src/main/java/com/books/library/author/AuthorController.java
@@ -1,6 +1,6 @@
 package com.books.library.author;
 
-import com.books.library.book.BookController;
+import com.books.library.book.BookService;
 import com.books.library.subject.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +10,17 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/author")
 public class AuthorController {
+
+    //region members
     @Autowired
     private AuthorService authorService;
     @Autowired
+    private BookService bookService;
+    @Autowired
     private SubjectService subjectService;
+    //endregion
 
+    //region endpoints
     @GetMapping
     public List<Author> getAuthors() {
         return authorService.getAuthors();
@@ -22,13 +28,16 @@ public class AuthorController {
 
     @PostMapping
     public void createAuthor(@RequestBody AddAuthorRequest request) {
-        this.authorService.createAuthor(request.forename(), request.surname(), request.subjectIds());
+        this.authorService.createAuthor(request.forename(), request.surname(), request.bookIds(), request.subjectIds());
     }
 
     @DeleteMapping(path = "{authorId}")
     public void deleteAuthor(@PathVariable("authorId") Integer id) {
         this.authorService.deleteAuthor(id);
     }
+    //endregion
 
-    public record AddAuthorRequest(String forename, String surname, List<Integer> subjectIds){};
+    //region record classes
+    public record AddAuthorRequest(String forename, String surname, List<Integer> bookIds, List<Integer> subjectIds){};
+    //endregion
 }
--- src/main/java/com/books/library/author/AuthorService.java
+++ src/main/java/com/books/library/author/AuthorService.java
@@ -1,27 +1,45 @@
 package com.books.library.author;
 
+import com.books.library.book.Book;
+import com.books.library.book.BookRepository;
 import com.books.library.subject.Subject;
 import com.books.library.subject.SubjectRepository;
 import com.books.library.subject.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
 public class AuthorService {
+
+    //region members
     @Autowired
     private AuthorRepository authorRepository;
     @Autowired
+    private BookRepository bookRepository;
+    @Autowired
     private SubjectRepository subjectRepository;
+    Author author = new Author();
+    //endregion
 
+    //region CRUD methods
     public List<Author> getAuthors() {
         return authorRepository.findAll();
     }
 
-    public void createAuthor(String forename, String surname, List<Integer> subjectIds) {
-        Author author = new Author();
+    public void createAuthor(String forename, String surname, List<Integer> bookIds, List<Integer> subjectIds) {
+
         author.setForename(forename);
         author.setSurname(surname);
+        for (Integer bookId:bookIds) {
+            Book book = bookRepository.findById(bookId).orElse(null);
+            if (book != null) {
+                author.getBooks().add(book);
+            } else {
+                //handling exception
+            }
+        }
         for (Integer subjectId: subjectIds) {
             Subject subject = subjectRepository.findById(subjectId).orElse(null);
             if (subject != null) {
@@ -36,4 +54,5 @@ public class AuthorService {
     public void deleteAuthor(Integer id) {
         this.authorRepository.deleteById(id);
     }
+    //endregion
 }
